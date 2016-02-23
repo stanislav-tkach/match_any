353961b96aad29a55beb6d478961b67b80b1defe
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-24 00:18:57 +0200

    match_any! implementation

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+
+macro_rules! match_any {
+    ($a:ident => $($x:ident: $t:ty => $b:block),+) => {
+        {
+            $(
+                if let Some($x) = $a.downcast_ref::<$t>() {
+                    $b
+                };
+            )+
+        }
+    }
+}
