fefa542c6690a180456cea994bcacce1772b9511
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-25 00:52:25 +0200

    Test has been updated

--- tests/main.rs
+++ tests/main.rs
@@ -8,11 +8,25 @@ fn make_any<T: Any>(value: T) -> Box<Any> {
 
 #[test]
 fn match_i32() {
+    let mut i32_matched = false;
+
     match_any!(make_any(10) => 
+        _x: i8 => { 
+            assert!(false);
+        }
+        _x: i16 => { 
+            assert!(false);
+        }
+        _x: i64 => { 
+            assert!(false);
+        }
         x: i32 => { 
             assert_eq!(*x, 10);
+            i32_matched = true;
         }
     );
+
+    assert!(i32_matched);
 }
 
 // TODO: Add #[bench].
